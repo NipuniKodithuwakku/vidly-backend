07ec72b64bf05ba074150379fadc3888ec5dcc93
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-01 11:12:54 +0530

    create update genre

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const Joi = require("joi");
 const app = express();
 app.use(express.json());
 
-const movies = [
+const genres = [
   { id: 1, name: "Action" },
   { id: 2, name: "Horror" },
   { id: 3, name: "Romance" },
 ];
 //get request
 
-app.get("http://vidly.com/api/", (req, res) => {
+app.get("/", (req, res) => {
   res.send(genres);
 });
 
@@ -19,13 +20,37 @@ app.post("/api/genres", (req, res) => {
     res.status(400).send("Name is required and should be minimum 3 characters");
   }
   return;
-  const movie = {
-    id: movies.length + 1,
+  const genre = {
+    id: genres.length + 1,
     name: req.body.name,
   };
-  movies.push(movie);
-  res.send(movie);
+  genres.push(genre);
+  res.send(genre);
 });
+
+//put request
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre) res.status(404).send("The genre with the given id was not found");
+
+  const result = validateGenre(req.body);
+  const { error } = validateGenre(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+//delete request
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.String().min(3).required(),
+  };
+  return Joi.validate(genre.schema);
+}
 
 const port = process.env.PORT || 3000;
 // const port = process.env.PORT;
