e62364bfd9cde8b4961ada33015a6766f53b04be
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-04 09:32:51 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const express = require("express");
+const app = express();
+
 mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("connected to the mongodb"))
   .catch((err) => console.error("could not connected to the mongodb"));
+
+app.use(express.json());
+app.use("/api/genres", genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on ${port}`));
